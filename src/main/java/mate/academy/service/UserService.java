497262d99e0834f7d2c3017dac2f74f47556ba79
DaserSix <sergie3005@gmail.com>
2026-03-13 00:47:17 +0200

    made indentification by email and authentification service.

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -8,13 +8,29 @@ public class UserService {
             new User("alice@i.ua", "1234")
     };
 
+    private static User indentifiedUser;
+
+    public static User getIndentifiedUser() {
+        return indentifiedUser;
+    }
+
+    public static void setIndentifiedUser(User indentifiedUser) {
+        UserService.indentifiedUser = indentifiedUser;
+    }
+
     /**
      * Find user by email. All users are stored in <code>private static final User[] users</code>
      * @param email - the input parameter
      * @return - user if his email is equal to passed email.
      * Return <code>null</code> if there is no suitable user
      */
+
     public User findByEmail(String email) {
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                setIndentifiedUser(user);
+            }
+        }
         return null;
     }
 }
